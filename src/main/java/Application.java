1243ff00cd1561ca0d5b05b401949962f60a82ac
Sanjiv Kumar <sanjiv.kumar34@gmail.com>
2017-04-16 00:25:51 +0530

    Configuring the drfa-portal

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,17 @@
 /**
  * Created by Sanjiv on 4/15/2017.
  */
+import com.imens.drfa.web.UploadingController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.File;
+import java.io.IOException;
+
+@SpringBootApplication
 public class Application {
+    public static void main(String[] args) throws IOException {
+        new File(UploadingController.uploadingdir).mkdirs();
+        SpringApplication.run(Application.class, args);
+    }
 }
